1d39381d968e4a0a13fcb9e5cd491bab3136b4a1
Hamza <Hamza.Alam@accrualgroup.com>
2022-04-28 14:02:54 +0500

    FrontEnd

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,7 +142,80 @@ $(".newsSlider").slick({
     // instead of a settings object
   ],
 });
-$(".orderSection .tabDiv ul li").click(function(){
+$(".recentBlogSlider").slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+$(".fixedServiceSliderDiv").slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+$(".orderSection .tabDiv ul li").click(function () {
   $(".orderSection .tabDiv ul li").removeClass("active");
-  $(this).addClass("active")
-})
+  $(this).addClass("active");
+});
+var input = document.querySelector("#phone");
+window.intlTelInput(input, {
+  separateDialCode: true,
+});
+$(document).ready(function () {
+  $(".js-example-basic-multiple").select2();
+});

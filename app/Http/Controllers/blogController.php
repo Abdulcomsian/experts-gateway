4cc782dd082d01b978a2e4a4718000d5361b82a1
obiafridi <obaid.afridi@accrualhub.com>
2022-07-20 17:24:03 +0500

    changes done

--- app/Http/Controllers/blogController.php
+++ app/Http/Controllers/blogController.php
@@ -69,7 +69,7 @@ class blogController extends Controller
             $extensions =$image->extension();
 
             $image_name =time().'.'. $extensions;
-            $image->move('blogs/',$image_name);
+            $image->move(public_path('blogs/'),$image_name);
             $blog->image=$image_name;
         }
         $blog->status=1;
@@ -102,7 +102,7 @@ class blogController extends Controller
             $extensions =$image->extension();
 
             $image_name =time().'.'. $extensions;
-            $image->move('blogs/',$image_name);
+            $image->move(public_path('blogs/'),$image_name);
             $blog->image=$image_name;
         }
         $blog->save();

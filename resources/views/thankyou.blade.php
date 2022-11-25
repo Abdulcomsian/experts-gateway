dfc202349bc3af3890e6af7bfa38b9822e375ddf
Abdul Basit <basitawan.abdul@gmail.com>
2022-11-25 13:08:04 +0500

    changes done

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -381,7 +381,7 @@ svg {
     <div class="absolute-top-left margin-8 flex-column-centered tablet-relative">
         <a href="{{url('/')}}" class="icon-button flex-row radius-full w-inline-block w--current">
             <img src="https://assets.website-files.com/5fb73a40b459b8605103611a/5fdb86b226732ad1c95ae64c_chevron_left_24px.svg" loading="lazy" alt="" class="width-4 margin-right-1">
-            <div class="text-color-white text-small">Back to Expert Gateway</div>
+            <div class="text-color-white text-small">Back to Experts Gateway</div>
         </a>
     </div>
     <img src="https://assets.website-files.com/5fb73a40b459b8605103611a/5fdd89a314b4b9a32ca1696e_sign-up-background.svg" loading="lazy" alt="" class="absolute-right-middle-below opacity-10 width-viewport-half">
@@ -389,7 +389,7 @@ svg {
         <img src="https://assets.website-files.com/5fb73a40b459b8605103611a/5fdb70b4ff7820da2f76e1a5_outseta-logomark.svg" loading="lazy" width="48" alt="" class="margin-bottom-6">
         <h3 class="text-color-white margin-bottom-12">Great! Now check your inbox.</h3>
         <div class="text-medium margin-bottom-6 w-richtext">
-            <p>We just sent you an email to complete the sign-up process &amp; set your password. Please check your spam folder if you haven’t received it within a few minutes.</p><p>Need some help? Shoot us a note at support@expertgateay.com. We're here to help!</p></div>
+            <p>We just sent you an email to complete the sign-up process &amp; set your password. Please check your spam folder if you haven’t received it within a few minutes.</p><p>Need some help? Shoot us a note at support@expertsgateay.com. We're here to help!</p></div>
 </div>
 </div>
 </body>

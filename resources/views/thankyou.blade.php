965697390a32bb175dc9aed7eec141bbb2d5ca95
Abdul Basit <basitawan.abdul@gmail.com>
2022-11-25 13:05:04 +0500

    thank you page done

---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,396 @@
+<html data-wf-domain="http://experts-gateway.com/" >
+ <head>
+    <style>.wf-force-outline-none[tabindex="-1"]:focus{outline:none;}</style>
+    <meta charset="utf-8"><title>Thank You | We appreciate you</title>
+     <meta content="If you ended up here, you signed up for experts-gateway. We love you. Preciate it. You gangsta you." name="description">
+     <link href="https://assets.website-files.com/5fb73a40b459b8605103611a/css/outseta.webflow.0b06762b4.min.css" rel="stylesheet" type="text/css">
+
+</head>
+<body class="background-color-nightfall"><div class="global-styles w-embed">
+<style>
+
+/* Make text look crisper and more legible in all browsers */
+body {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+}
+
+/* Focus state style for keyboard navigation for the focusable elements */
+*[tabindex]:focus-visible,
+  input[type="file"]:focus-visible {
+   outline: 0.125rem solid #4d65ff;
+   outline-offset: 0.125rem;
+}
+
+/* Get rid of top margin on first element in any rich text element */
+.w-richtext > :not(div):first-child, .w-richtext > div:first-child > :first-child {
+  margin-top: 0 !important;
+}
+
+/* Get rid of bottom margin on last element in any rich text element */
+.w-richtext>:last-child, .w-richtext ol li:last-child, .w-richtext ul li:last-child {
+	margin-bottom: 0 !important;
+}
+
+/* Prevent all click and hover interaction with an element */
+.pointer-events-off {
+	pointer-events: none;
+}
+
+/* Enables all click and hover interaction with an element */
+.pointer-events-on {
+  pointer-events: auto;
+}
+
+/* Create a class of .div-square which maintains a 1:1 dimension of a div */
+.div-square::after {
+	content: "";
+	display: block;
+	padding-bottom: 100%;
+}
+
+/* Make sure containers never lose their center alignment */
+.container-medium, .container-small, .container-large {
+	margin-right: auto !important;
+  margin-left: auto !important;
+}
+
+
+
+/* Apply "..." after 3 lines of text */
+.text-style-3lines {
+	display: -webkit-box;
+	overflow: hidden;
+	-webkit-line-clamp: 3;
+	-webkit-box-orient: vertical;
+}
+
+/* Apply "..." after 2 lines of text */
+.text-style-2lines {
+	display: -webkit-box;
+	overflow: hidden;
+	-webkit-line-clamp: 2;
+	-webkit-box-orient: vertical;
+}
+
+/* Adds inline flex display */
+.display-inlineflex {
+  display: inline-flex;
+}
+
+
+.margin-0 {
+  margin: 0rem !important;
+}
+  
+.padding-0 {
+  padding: 0rem !important;
+}
+
+.spacing-clean {
+padding: 0rem !important;
+margin: 0rem !important;
+}
+
+.margin-top {
+  margin-right: 0rem !important;
+  margin-bottom: 0rem !important;
+  margin-left: 0rem !important;
+}
+
+.padding-top {
+  padding-right: 0rem !important;
+  padding-bottom: 0rem !important;
+  padding-left: 0rem !important;
+}
+  
+.margin-right {
+  margin-top: 0rem !important;
+  margin-bottom: 0rem !important;
+  margin-left: 0rem !important;
+}
+
+.padding-right {
+  padding-top: 0rem !important;
+  padding-bottom: 0rem !important;
+  padding-left: 0rem !important;
+}
+
+.margin-bottom {
+  margin-top: 0rem !important;
+  margin-right: 0rem !important;
+  margin-left: 0rem !important;
+}
+
+.padding-bottom {
+  padding-top: 0rem !important;
+  padding-right: 0rem !important;
+  padding-left: 0rem !important;
+}
+
+.margin-left {
+  margin-top: 0rem !important;
+  margin-right: 0rem !important;
+  margin-bottom: 0rem !important;
+}
+  
+.padding-left {
+  padding-top: 0rem !important;
+  padding-right: 0rem !important;
+  padding-bottom: 0rem !important;
+}
+  
+.margin-horizontal {
+  margin-top: 0rem !important;
+  margin-bottom: 0rem !important;
+}
+
+.padding-horizontal {
+  padding-top: 0rem !important;
+  padding-bottom: 0rem !important;
+}
+
+.margin-vertical {
+  margin-right: 0rem !important;
+  margin-left: 0rem !important;
+}
+  
+.padding-vertical {
+  padding-right: 0rem !important;
+  padding-left: 0rem !important;
+}
+  
+/* Modifying number of items in a list within a component */
+  
+[data-items="0"] li:nth-child(n+1) {
+    display: none;
+}
+  
+[data-items="1"] li:nth-child(n+2) {
+    display: none;
+}
+  
+  [data-items="2"] li:nth-child(n+3) {
+    display: none;
+  }
+  
+  [data-items="3"] li:nth-child(n+4) {
+    display: none;
+  }
+  
+  /* Hide/show back to blog button in blog navigation */
+  
+  [data-back-to-blog="show"] .back-to-blog {
+    display: flex;
+  }
+  
+  /* Adding calc to subtract the header from viewport height */
+  
+  .height-viewport-full-minus-header, .feature-header_component {
+    min-height: calc(100vh - 4rem);
+  }
+  
+  @media screen and (max-width: 991px) {
+    .height-viewport-full-minus-header, .feature-header_component {
+      min-height: auto;
+    }
+  }
+  
+  /* Making the mobile nav menu the right height */
+  
+  @media screen and (max-width: 767px) {
+    .nav-menu {
+      height: calc(100vh - 3.25rem);
+    }
+  }
+  
+  /* Custom unordered and ordered lists */
+  
+  .w-richtext ul {
+    padding-left: 1.5em;
+  }
+  
+  .w-richtext ul li::before {
+    content: '';
+    display: inline-block;
+    height: 1em;
+    width: 1.5em;
+    margin-left: -1.5em;
+    position: relative;
+    top: 3px;
+    background-image: url(https://assets.website-files.com/5fb73a40b459b8605103611a/5fcbed131d0e59c191bd8e45_Check%20Circle%20-%20Branded.svg);
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+  
+  .w-richtext ul li::marker {
+    content: '';
+  }
+  
+  .w-richtext ol li {
+    counter-increment: list;
+  }
+  
+  .w-richtext ol li::before {
+    content: counter(list) ". ";
+    display: inline-block;
+    margin-left: -1.25em !important;
+    min-width: 0.75em !important;
+    text-align: right !important;
+    position: relative;
+    left: -0.75em;
+  }
+  
+  .w-richtext ol {
+    list-style: none;
+    counter-reset: list;
+  }
+  
+  /* Fixing video aspect ratio */
+  
+  .w-video, .w-richtext figure.w-richtext-figure-type-video, .w-richtext figure[data-rt-type=video] {
+    padding-top: 62.5% !important;
+    padding-bottom: 0 !important;
+  }
+  
+  /* Email sign up embed */
+  
+  .email-sign-up {
+  text-align: center;
+  }
+  
+  .email-sign-up .form-group input[type="email"] {
+  padding: 12px 16px;
+  border-radius: 4px;
+  border: 1px solid rgba(0,0,0,.1);
+  color: #002129;
+  margin-bottom: 1em;
+  min-width: 50%;
+  }
+  
+  .email-sign-up .form-group input[type="email"]:focus {
+  border: 1px solid #0098AA;
+  outline: none;
+  }
+  
+  .email-sign-up .btn {
+  color: #ffffff;
+  border: none !important;
+  }
+  
+  .email-sign-up .btn:hover {
+  border: 1px solid #1B6567;
+  }
+  
+  .email-sign-up .btn:focus {
+  outline: none;
+  }
+  
+  ::placeholder {
+  color: rgba(0, 33, 41, 0.3);
+  }
+  
+  .email-sign-up .form-group .g-recaptcha {
+  margin-bottom: 1em;
+  }
+  
+  /* Fix for when a heading is the first element in a line height 1.75 text block */
+  
+  .line-height-1-75 :first-child {
+    margin-top: 0;
+  }
+  
+  /* Adding inner border to blog images */
+  
+  figure div, .image-border {
+    position: relative;
+  }
+  
+  figure div:after, .image-border:after {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    box-shadow: inset 0 0 0 1px rgba(0,0,0,.1);
+    border-radius: .5rem;
+  }
+  
+  /* Adding a property to hide horizontal overflow */
+  
+  .overflow-x-hidden {
+    overflow-x: hidden;
+  }
+  
+  /* Fix for embed incompatibility with overflow-y: scroll */
+  
+  html {
+    overflow-y: visible !important;
+  }
+  
+  /* Add screen blend mode */
+  .screen {
+    -webkit-mix-blend-mode: screen;
+    mix-blend-mode: screen;
+  }
+  
+  /* Add outline pseudo element */
+  .text-highlight:after {
+    top: -.25em;
+    left: -.25em;
+    right: -.25em;
+    bottom: -.25em;
+    background-position: center;
+    background-size: 100% 100%;
+    position: absolute;
+    z-index: -1;
+    content: '';
+    background-repeat: no-repeat;
+  }
+  
+  .text-highlight.circle:after {
+    background-image: url('https://assets.website-files.com/5fb73a40b459b8605103611a/60f4936842eda3c982b45671_circle.svg');
+  }
+  
+  .text-highlight.smooth-underline:after {
+    background-image: url('https://assets.website-files.com/5fb73a40b459b8605103611a/60f494bcb62a56827568a520_smooth-underline.svg');
+  }
+  
+  .text-highlight.wiggly-underline:after {
+    background-image: url('https://assets.website-files.com/5fb73a40b459b8605103611a/60f494c16827eadf02e8d57e_wiggly-underline.svg');
+  }
+  
+svg {
+  display: block;
+}
+
+.is-pinned {
+  opacity: 100%;
+}
+
+.dev-workflows_anchor-link:hover {
+    opacity: 1 !important;
+}
+
+</style>
+</div>
+<div class="section-in-base height-viewport-full flex-align-center tablet-flex-column overflow-hidden relative wf-section">
+    <div class="absolute-top-left margin-8 flex-column-centered tablet-relative">
+        <a href="{{url('/')}}" class="icon-button flex-row radius-full w-inline-block w--current">
+            <img src="https://assets.website-files.com/5fb73a40b459b8605103611a/5fdb86b226732ad1c95ae64c_chevron_left_24px.svg" loading="lazy" alt="" class="width-4 margin-right-1">
+            <div class="text-color-white text-small">Back to Expert Gateway</div>
+        </a>
+    </div>
+    <img src="https://assets.website-files.com/5fb73a40b459b8605103611a/5fdd89a314b4b9a32ca1696e_sign-up-background.svg" loading="lazy" alt="" class="absolute-right-middle-below opacity-10 width-viewport-half">
+    <div class="text-align-center text-color-white container-480 w-container">
+        <img src="https://assets.website-files.com/5fb73a40b459b8605103611a/5fdb70b4ff7820da2f76e1a5_outseta-logomark.svg" loading="lazy" width="48" alt="" class="margin-bottom-6">
+        <h3 class="text-color-white margin-bottom-12">Great! Now check your inbox.</h3>
+        <div class="text-medium margin-bottom-6 w-richtext">
+            <p>We just sent you an email to complete the sign-up process &amp; set your password. Please check your spam folder if you haven’t received it within a few minutes.</p><p>Need some help? Shoot us a note at support@expertgateay.com. We're here to help!</p></div>
+</div>
+</div>
+</body>
+</html>
